85874de3a42b4644ea1a898d53ace163d512a6fc
Adrian Tse <tse.adrian@gmail.com>
2019-06-25 08:10:18 +1000

    Dashboard updates

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+sudo apt-get install snmp snmp-mibs-downloader
+
 echo "deb https://repos.influxdata.com/debian stretch stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
-sudo apt-get update && sudo apt-get install influxdb telegraf -y 
+sudo apt-get update && sudo apt-get install telegraf -y 
